bc1a2f71b74d8f03feecfb06aab9696015565923
Wyndham Hudson <whudson05@gmail.com>
2014-12-01 23:09:32 +0000

    Added ng app

--- client/js/controllers/itemsController.js
+++ client/js/controllers/itemsController.js
@@ -1,12 +1,12 @@
-function itemsController($scope) {
+app.controller('itemsController', ['$scope', '$resource', function ($scope, $resource) {   
     $scope.itemsCount = 1;
-    
+
     $scope.items = [
         { name : "First item" },
         { name : "Second item" }
     ];
-    
+
     $scope.createItem = function () {
         $scope.items.push({ name: $scope.itemName });
     };
-}
+}]);
