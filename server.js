45446f2dae2672a21fc521ab7e80a18662b9e475
Wyndham Hudson <whudson05@gmail.com>
2014-12-01 22:46:28 +0000

    Got AngularJS working
    Will need to update Angular to latest version as it’s some way behind.

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express'),
     app = express();
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/client/views/index.html');
+    res.sendFile(__dirname + '/client/views/index.html');
 });
+
+app.use('/js/', express.static(__dirname + '/client/js/'));
 
 app.listen(3000, function () {
     console.log('I\'m listening');
